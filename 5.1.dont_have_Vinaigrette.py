62bbd953ed06a09a97f9b67ec180b94a134f6f9e
Roee <roibenezra0@gmail.com>
2023-03-17 15:38:31 +0200

    5.1, 5.2 done

--- 5.1.dont_have_Vinaigrette.py
+++ 5.1.dont_have_Vinaigrette.py
@@ -1,20 +1,5 @@
-import os
 import random
 from datetime import datetime, timedelta
-
-
-def that_is_the_way(path):
-    """
-    The function that_is_the_way takes a path as an argument and returns
-     a list of files in that path that contain the substring 'deep'.
-    :param path: path of directory to search in
-    :return: list of matches
-    """
-    return [file for file in os.listdir(path) if 'deep' in file]
-
-
-# print the search result
-print(that_is_the_way(os.path.join('images')))
 
 
 def dont_have_Vinaigrette(start_d, end_d):
